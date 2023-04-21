a40bc945e1aec87cfb4080377ab1ba6549edc05d
Santtu Jaaksola <santtu.jaaksola@gmail.com>
2023-04-21 12:46:51 +0300

    Added config template file

---
+++ jb-config-template.php
@@ -0,0 +1,7 @@
+<?php
+
+define("CONSUMER_KEY","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+define("CONSUMER_SECRET","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+define("BEARER_TOKEN","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+define("ACCESS_TOKEN","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+define("ACCESS_TOKEN_SECRET","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
